cfb9b18929e84e1574ab04c059fb1de1379064f3
Rokas Andreikenas <rokas@10speed.com>
2022-02-03 18:21:46 +0200

    add map for my location

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 fetch("https://api.meteo.lt/v1/places")
-  .then((response) => response.json())
+  .then((response) => {
+    const responseInput = document.getElementById("response");
+    const status = response.statusText;
+    responseInput.innerText = `response: ${status}`;
+    return response.json();
+  })
   .then((placesJson) => {
     const selectInput = document.getElementById("places");
 
-    placesJson.length = 100;
-
     placesJson.forEach((place) => {
-      const optionNote = document.createElement("option");
+      const optionNode = document.createElement("option");
 
-      optionNote.innerText = place.name;
-      optionNote.value = place.code;
+      optionNode.innerText = place.name;
+      optionNode.value = place.code;
 
-      selectInput.append(optionNote);
+      selectInput.append(optionNode);
     });
   });
 
@@ -31,3 +34,33 @@ const onSelect = () => {
 };
 
 selectInput.addEventListener("change", onSelect);
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      initMap(latitude, longitude);
+    });
+  } else {
+    console.log("Browser does not support geo location");
+  }
+}
+
+function initMap(latitude, longitude) {
+  if (latitude && longitude) {
+    const iframeNode = document.createElement("iframe");
+    const url = `http://maps.google.com/maps?q=${latitude},${longitude}&z=16&output=embed`;
+    iframeNode.src = url;
+    iframeNode.style = "border: 0; margin-bottom: 24px";
+    iframeNode.width = 450;
+    iframeNode.height = 300;
+    iframeNode.setAttribute("loading", "lazy");
+    iframeNode.setAttribute("allowfullscreen", "");
+
+    const mapDiv = document.getElementById("map");
+    mapDiv.append(iframeNode);
+  }
+}
+
+getLocation();
